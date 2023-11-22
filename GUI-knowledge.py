daae94e912bbb3098ef1809d88d46f5a54051ee9
ouyouyouy <148435297+ouyouyouy@users.noreply.github.com>
2023-11-22 23:51:46 +0700

    update ver2.0

--- GUI-knowledge.py
+++ GUI-knowledge.py
@@ -1,5 +1,12 @@
 from tkinter import * # import all function inside main package
 from tkinter import ttk
+import csv
+
+def writecsv(data):
+    # data = ['john',14,500]
+    with open ('knowledge.csv','a',newline='',encoding='utf-8') as file:
+        fw = csv.writer(file) # fw = file writer
+        fw.writerow(data)
 #from tkinter import Button , Tk , Entry เราไม่อยากพิมพ์เยอะ จึงใช้ import *
 # การ import tkinter เวลาเราเรียกใช้ จะต้องพิมพ์ คำว่า tkinter.Button() , tkinter.Entry() 
 # แต่ถ้า ใช้ from tkinter import * เราจะเรียกใช้ Button() , Entry() ได้เลย ง่ายกว่า
@@ -12,7 +19,8 @@ GUI.geometry('500x500')
 L1 = ttk.Label(GUI,text='หัวข้อความรู้',font = ('Angsana New',18))
 L1.pack()
 
-E1 = ttk.Entry(GUI,font=('Angsana New',20),width=50) # grid , pack ,place
+v_title = StringVar()
+E1 = ttk.Entry(GUI, textvariable = v_title ,font=('Angsana New',20),width=50) # grid , pack ,place
 E1.pack()
 #E1.place(x=300,y=300)
 
@@ -22,8 +30,23 @@ L2.pack()
 T1 = Text(GUI,font=('Angsana New',18),height=8,width=56)
 T1.pack()
 
+def save():
+    title = v_title.get()
+    textbox = T1.get(1.0,'end-1c')
+    print('------')
+    print(title)
+    print('------')
+    print(textbox)
+    print('------')
+    # print([textbox]) # การ print โดยใส่ [] เราจะเห็นสิ่งที่ซ่อนอยู่ภายใน
+    data = [title,textbox]
+    writecsv(data)
+    v_title.set('') # clear data after save
+    T1.delete('1.0',END) # clear text box
 
-B1 = ttk.Button(GUI,text='บันทึก')
+
+
+B1 = ttk.Button(GUI,text='บันทึก',command=save)
 B1.pack(pady=10,ipadx=20,ipady=10)
 
 GUI.mainloop() # mainloop คือ การรันโปรแกรมโดยไม่ปิด คล้ายๆ while True
